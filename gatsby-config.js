b73d65d32f339a98cda85c2770f954b9aa540272
Henry Seongwook Ha <swh0901@gmail.com>
2022-05-05 02:09:31 +0900

    tsconfig and change config to js

---
+++ gatsby-config.js
@@ -0,0 +1,39 @@
+const config = {
+  siteMetadata: {
+    title: `Henry's Devlog`,
+    siteUrl: `https://henrysha.github.io`,
+  },
+  plugins: [
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.png',
+      },
+    },
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: './src/pages/',
+      },
+      __key: 'pages',
+    },
+  ],
+}
+
+export default config
