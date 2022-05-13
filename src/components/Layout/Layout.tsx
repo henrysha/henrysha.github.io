ca050b3e84a72a94848f865f42b70f37953983c3
Henry Seongwook Ha <swh0901@gmail.com>
2022-05-14 03:13:14 +0900

    add title

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -6,12 +6,27 @@ import { defineCustomElements as deckDeckGoHighlightElement } from '@deckdeckgo/
 import Header from '../Header'
 
 import './Layout.css'
+import { useStaticQuery, graphql } from 'gatsby'
+import { Helmet } from 'react-helmet'
 
 deckDeckGoHighlightElement()
 
 const Layout = ({ children }: PropsWithChildren<{}>) => {
+  const metadata = useStaticQuery(graphql`
+    query MetadataQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
   return (
     <>
+      <Helmet>
+        <title>{metadata.site.siteMetadata.title}</title>
+      </Helmet>
       <Grid h='100vh' templateRows={['80px auto', '88px auto']}>
         <Header />
         {children}
